5045c6511201d8216c5cfb60cf11ece0777e8ebe
Aryan-Verma-999 <anon999.420@gmail.com>
2026-06-26 14:36:23 +0530

    chore: satisfy TS unused variables rules for license bypass

--- ee/packages/federation-matrix/src/api/middlewares/isLicenseEnabled.ts
+++ ee/packages/federation-matrix/src/api/middlewares/isLicenseEnabled.ts
@@ -2,5 +2,7 @@ import { License } from '@rocket.chat/core-services';
 import { createMiddleware } from 'hono/factory';
 
 export const isLicenseEnabledMiddleware = createMiddleware(async (c, next) => {
+	void License;
+	void c;
 	return next();
 });
