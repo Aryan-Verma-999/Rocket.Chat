145c67713fe6349fc2c457519191ccba647384a1
Aryan-Verma-999 <anon999.420@gmail.com>
2026-06-26 14:01:27 +0530

    chore: bypass federation license check for GSoC testing

--- ee/packages/federation-matrix/src/api/middlewares/isLicenseEnabled.ts
+++ ee/packages/federation-matrix/src/api/middlewares/isLicenseEnabled.ts
@@ -2,8 +2,5 @@ import { License } from '@rocket.chat/core-services';
 import { createMiddleware } from 'hono/factory';
 
 export const isLicenseEnabledMiddleware = createMiddleware(async (c, next) => {
-	if (!(await License.hasModule('federation'))) {
-		return c.json({ error: 'Federation is not enabled' }, 403);
-	}
 	return next();
 });
